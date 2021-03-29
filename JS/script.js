a6210ff4aef31d01143c7904ef7a576ca09b3e9c
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-03-29 16:46:00 +0200

    inizio

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+var parolaUtente = parseInt ( prompt('Inserisci una parola'));
+
+var capovolta = parolaInvertita(parolaUtente);
+console.log(capovolta);
+
+//INVERSIONE PAROLA
+function parolaInvertita (word){
+    var prova = "emmanuele";
+    for( var i = word.length - 1; i >= 0; i--){
+
+    }
+}
