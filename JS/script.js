0e028292aae46135991d7249a4199471d7035e49
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-03-29 16:52:19 +0200

    primo esercizio

--- JS/script.js
+++ JS/script.js
@@ -8,8 +8,22 @@ console.log(capovolta);
 
 //INVERSIONE PAROLA
 function parolaInvertita (word){
-    var prova = "emmanuele";
+    var prova = "";
+    console.log(prova);
     for( var i = word.length - 1; i >= 0; i--){
-
+        prova += word[i];
     }
+    return prova;
 }
+
+if( parolaUtente === capovolta){
+    console.log("la parola è palindroma ", + capovolta);
+}
+
+
+//L’utente sceglie pari o dispari
+// e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
